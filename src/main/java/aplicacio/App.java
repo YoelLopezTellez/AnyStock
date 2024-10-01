6c780bf692f8d06db393324f55c5d67c03f0326e
Reyes <ramador@alumnat.copernic.cat>
2024-10-01 11:58:02 +0200

    arreglado bug de .App

--- src/main/java/aplicacio/App.java
+++ src/main/java/aplicacio/App.java
@@ -27,7 +27,7 @@ public class App extends Application {
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/cat/copernic/projecte_grup4/" + fxml + ".fxml"));
         return fxmlLoader.load();
     }
 
