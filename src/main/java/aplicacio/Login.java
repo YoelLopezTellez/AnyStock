fae761b1747368e72eb3ecec7f2c25dfef7ace76
Reyes <ramador@alumnat.copernic.cat>
2024-10-02 10:27:36 +0200

    modificando la clase login para añadir Map

--- src/main/java/aplicacio/Login.java
+++ src/main/java/aplicacio/Login.java
@@ -8,41 +8,55 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Login per comprovar que l'usuari i contrasenya existeixen en un fitxer dintre de resources
  * @author reyes
+ * @version 0.2
  */
 public class Login {
     
-    private String ruta;
+    private Map<String, Usuari> usuaris = new HashMap<>();
 
-    public Login(String ruta) {
-        this.ruta = ruta;
+    public Login(InputStream ruta) throws IOException {
+        cargarUsuarios(ruta);
+    }
+    
+    private void cargarUsuarios(InputStream ruta) throws IOException{
+        BufferedReader fitxer = new BufferedReader(new InputStreamReader(ruta));
+        String linea;
+        while((linea = fitxer.readLine()) != null){
+            String[] credenciales = linea.split(","); //separem les credencials ","
+            if(credenciales.length == 3){
+                String nom = credenciales[0].trim();
+                String contra = credenciales[1].trim();
+                String rol = credenciales[2].trim();
+                
+                // com el Map agafa tan sols Usuari, necesitem especificar que usuari es d'una subclase dintre de la clase Usuari
+                //ja que si fiquem tan sols una de les subclases, no ens deixará ficar-la en el Map usuaris
+                Usuari usuari;
+                
+                if(rol.equalsIgnoreCase("Responsable"))
+                    usuari = new Responsable(nom,contra);
+                else if(rol.equalsIgnoreCase("Vendedor"))
+                    usuari = new Vendedor(nom,contra);
+                else
+                    continue;
+                
+                
+            }
+        }
     }
     
     //retorna true si les credencials son correctes, false si no
     public boolean autentificacio(String nom, String pass){
         //utilitzem inputStream perque l'arxiu estarà empaquetat en el JAR i necessitem llegir en binari
         //getClass retorna la clase, getClassLoader serveix per cargar fitxers de recursos, l'ultim localitza si existeix el fitxer, retorna un inputStream o un null
-        try(InputStream inputstream = getClass().getClassLoader().getResourceAsStream(ruta);
-                BufferedReader fitxer = new BufferedReader(new InputStreamReader(inputstream))){
-            String linea;
-            while((linea = fitxer.readLine()) != null){
-                String[] credenciales = linea.split(","); //separem les credencials "," 
-                if(credenciales.length == 3){
-                    String usuari = credenciales[0];
-                    String contra = credenciales[1];
-                    if(usuari.equals(nom) && contra.equals(pass)){
-                        return true;
-                    }
-                }
-            }
-        }catch(IOException e){
-            e.printStackTrace();
-        }
-        
+        //return usuarios.containsKey(nom) && usuarios.get(nom).
         return false;
     }
+    
     
 }
