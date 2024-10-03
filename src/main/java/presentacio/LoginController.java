1ddd168a3f0e535ae3e03135521667459531e9c7
Reyes <ramador@alumnat.copernic.cat>
2024-10-03 14:11:53 +0200

    funcionalidad del boton login y su comprobacion correcta

--- src/main/java/presentacio/LoginController.java
+++ src/main/java/presentacio/LoginController.java
@@ -5,6 +5,9 @@ import aplicacio.model.Usuari;
 import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
@@ -28,21 +31,29 @@ public class LoginController {
             // Obtener la ruta del archivo en resources y pasarlo a la clase Login
             //getClass retorna la clase, getClassLoader serveix per cargar fitxers de recursos, l'ultim localitza si existeix el fitxer
             //getResourceAsStream retorna inputStream per poder passar-lo al metode cargarUsuarios de Login
-            login = new Login(getClass().getClassLoader().getResourceAsStream("usuarios.txt"));
+            login = new Login(getClass().getClassLoader().getResourceAsStream("usuaris.txt"));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
     
-    /*@FXML
-    private onBtn1_action(ActionEvent event){
+    @FXML
+    private void onBtn1_action(ActionEvent event) throws IOException{
         String nom = tf_nom.getText();
         String pass = tf_pass.getText();
         
         Usuari usuari = login.autentificacio(nom, pass);
         
         if(usuari != null){
-            
+            cambiarPantalla();
         }
-    }*/
+    }
+    
+    //
+    private void cambiarPantalla() throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/cat/copernic/projecte_grup4/secondary.fxml"));
+        Parent novaPantalla = loader.load();
+        Scene escenaActual = tf_nom.getScene();
+        escenaActual.setRoot(novaPantalla);
+    }
 }
