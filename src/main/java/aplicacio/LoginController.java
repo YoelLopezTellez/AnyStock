2c5fec4d629649471395b8c1c3de852cab8bb4ef
Reyes <ramador@alumnat.copernic.cat>
2024-10-03 11:36:45 +0200

    provando opciones para cambiar pantalla

--- src/main/java/aplicacio/LoginController.java
+++ src/main/java/aplicacio/LoginController.java
@@ -1,6 +1,8 @@
 package aplicacio;
 
+import aplicacio.model.Usuari;
 import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -18,15 +20,28 @@ public class LoginController {
     
     private Login login;
     
+    //al carregar la escena ja executa els usuaris que hi ha al fitxer
     @FXML
     public void initialize() {
-    try {
-        // Obtener la ruta del archivo en resources y pasarlo a la clase Login
-        //getClass retorna la clase, getClassLoader serveix per cargar fitxers de recursos, l'ultim localitza si existeix el fitxer
-        //getResourceAsStream retorna inputStream per poder passar-lo al metode cargarUsuarios de Login
-        login = new Login(getClass().getClassLoader().getResourceAsStream("usuarios.txt"));
-    } catch (IOException e) {
-        e.printStackTrace();
+        try {
+            // Obtener la ruta del archivo en resources y pasarlo a la clase Login
+            //getClass retorna la clase, getClassLoader serveix per cargar fitxers de recursos, l'ultim localitza si existeix el fitxer
+            //getResourceAsStream retorna inputStream per poder passar-lo al metode cargarUsuarios de Login
+            login = new Login(getClass().getClassLoader().getResourceAsStream("usuarios.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-}
+    
+    /*@FXML
+    private onBtn1_action(ActionEvent event){
+        String nom = tf_nom.getText();
+        String pass = tf_pass.getText();
+        
+        Usuari usuari = login.autentificacio(nom, pass);
+        
+        if(usuari != null){
+            
+        }
+    }*/
 }
--- src/main/java/aplicacio/MenuController.java
+++ src/main/java/aplicacio/MenuController.java
@@ -3,7 +3,7 @@ package aplicacio;
 import java.io.IOException;
 import javafx.fxml.FXML;
 
-public class SecondaryController {
+public class MenuController {
 
     @FXML
     private void switchToPrimary() throws IOException {
