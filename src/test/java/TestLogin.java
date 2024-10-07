e90adfbfe53364927995bcdbee4a8d7d2a35022f
Reyes <ramador@alumnat.copernic.cat>
2024-10-07 10:20:39 +0200

    tests per login fets

---
+++ src/test/java/TestLogin.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+
+import aplicacio.model.TIPUSROL;
+import aplicacio.model.Usuari;
+import logica.Login;
+import logica.Sessio;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author reyes
+ */
+public class TestLogin {
+    
+    Login login;
+    
+    //comprova que l'usuari que hem ficat existeix en el fitxer amb el not null i també que el nom sigui correcte
+    @Test
+    public void testAutentificacio(){
+        Usuari usuari = login.autentificacio("reyes", "123");
+        assertNotNull(usuari);
+        assertEquals("reyes", usuari.getNomUsuari());
+    }
+    
+    //ponemos una contraseña incorrecta a un usuario que si existe para que nos devuelva un null
+    @Test
+    public void testPassIncorrecta(){
+        Usuari usuari = login.autentificacio("reyes", "unodostres");
+        assertNull(usuari);
+    }
+    
+    //posem un usuari que no existeix pero si una pass válida i ens ha de donar un null
+    @Test
+    public void testNomIncorrecte(){
+        Usuari usuari = login.autentificacio("sara", "123");
+        assertNull(usuari);
+    }
+    
+    //Assegurem que el metode de iniciar sessio retorni bé l'usuari ja que ha de ser igual un de l'altre
+    @Test
+    public void testIniciarSesion() {
+        Usuari usuario = new Usuari("reyes", "123", TIPUSROL.RESPONSABLE);
+        Sessio.getInstancia().iniciarSessio(usuario);
+        assertEquals(usuario, Sessio.getInstancia().getUsuari());
+    }
+    
+    //Testejem que cridant a tancar sessió ens retorni un null, així comprovem que funciona correctament el metode
+    @Test
+    public void testCerrarSesion() {
+        Sessio.getInstancia().tancarSessio();
+        assertNull(Sessio.getInstancia().getUsuari());
+    }
+    
+    //verifiquem que la sessio es la mateixa
+    @Test
+    public void testSingletonSessio(){
+        Sessio sessio1 = Sessio.getInstancia();
+        Sessio sessio2 = Sessio.getInstancia();
+        assertSame(sessio1,sessio2);
+    }
+}
