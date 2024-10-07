99a01dcbe585b1b09cd9a6093069de1133c53b6c
Mario Olaya Perez <molaya@alumnat.copernic.cat>
2024-10-07 09:36:57 +0200

    Comienzo controller referencia

--- src/main/java/presentacio/ConsultaReferenciaController.java
+++ src/main/java/presentacio/ConsultaReferenciaController.java
@@ -4,21 +4,25 @@
  */
 package presentacio;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import logica.CanviPantalla;
 
 /**
  * FXML Controller class
  *
  * @author mario
  */
-public class ConsultaReferenciaController implements Initializable {
+public class ConsultaReferenciaController{
 
     @FXML
     private Button btnNova;
@@ -89,12 +93,35 @@ public class ConsultaReferenciaController implements Initializable {
     @FXML
     private Button btnSortir;
 
-    
     /**
      * Initializes the controller class.
      */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    
+    @FXML
+    public void initialize() {
+        // Asociar las columnas de la tabla con los atributos de los items usando métodos tradicionales
+        colId.setCellValueFactory(new PropertyValueFactory<Item, String>("id"));
+        colNom.setCellValueFactory(new PropertyValueFactory<Item, String>("nom"));
+        colPreu.setCellValueFactory(new PropertyValueFactory<Item, String>("preu"));
+        colQuantitat.setCellValueFactory(new PropertyValueFactory<Item, String>("quantitat"));
+        colUom.setCellValueFactory(new PropertyValueFactory<Item, String>("uom"));
+        colProveidor.setCellValueFactory(new PropertyValueFactory<Item, String>("proveidor"));
+        colDataAlta.setCellValueFactory(new PropertyValueFactory<Item, String>("dataAlta"));
+
+        // Asignar la lista observable a la tabla
+        tableView.setItems(itemList);
+
+        // Agregar algunos elementos de ejemplo
+        itemList.add(new Item("1", "Producte 1", "10.0", "5", "uom1", "CIF123", "2023-10-01"));
+    }
+
+    @FXML
+    private void onbtnFamilia_Action(ActionEvent event) throws IOException {
+        CanviPantalla.canviarPantalla(btnFamilia.getScene(), "/cat/copernic/projecte_grup4/ConsultaFamilia.fxml");
+    }
+
+    @FXML
+    private void onbtnSortir_Action(ActionEvent event) throws IOException {
+        CanviPantalla.canviarPantalla(btnSortir.getScene(), "/cat/copernic/projecte_grup4/Menu.fxml");
+    }
 }
