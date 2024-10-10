3ece782f4286699ba5b4a12bf7969a67b13ece87
Mario Olaya Perez <molaya@alumnat.copernic.cat>
2024-10-10 08:15:30 +0200

    Todo referencias acabado

--- src/main/java/presentacio/Error.java
+++ src/main/java/presentacio/Error.java
@@ -11,7 +11,7 @@ import javafx.scene.control.Alert;
  * @author reyes
  */
 public class Error {
-    public void mostrarError(String titol, String missatge){
+    public static void mostrarError(String titol, String missatge){
         Alert alerta = new Alert(Alert.AlertType.ERROR);
         alerta.setTitle(titol);
         alerta.setHeaderText(null);
